a8ec831a82e617f996f4e88001a1a9f839cf62b5
Preetham <preethamb97@gmail.com>
2025-01-24 00:14:02 +0530

    hnages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ollama-UI-locally/',
+  base: '/ollama-UI-run/',
   plugins: [react()],
   server: {
     proxy: {
